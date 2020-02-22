41e438414cecfbea5758360ec983ba3fb77ec502
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-02-22 23:14:54 +0100

    first commit

---
+++ Program01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.Start();
+            Console.ReadKey();
+        }
+
+        void Start()
+        {
+
+        }
+    }
+}
