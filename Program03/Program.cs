3638ba6cf63d3b650a8c2f797a376dbca67affc1
stephenpangga <42969061+stephenpangga@users.noreply.github.com>
2020-02-26 15:10:07 +0100

    finished assignment 03

--- Program03/Program.cs
+++ Program03/Program.cs
@@ -20,6 +20,28 @@ namespace Program03
             RegularCandies[,] playingField = new RegularCandies[9,9];
             InitCandies(playingField);
             DisplayCandies(playingField);
+            bool scorerow = ScoreRowPresent(playingField);
+            bool scorecolumn = ScoreColumnPresent(playingField);
+
+            //this for the score row
+            if(scorerow)
+            {
+                Console.WriteLine("row score");
+            }
+            else
+            {
+                Console.WriteLine("no row score");
+            }
+
+            //message when there is a score in the column
+            if(scorecolumn)
+            {
+                Console.WriteLine("column score");
+            }
+            else
+            {
+                Console.WriteLine("no column  score");
+            }
 
         }
 
@@ -75,8 +97,87 @@ namespace Program03
         //still need to do question e and f
         bool ScoreRowPresent(RegularCandies[,] matrix)
         {
+            //this will be use as a counter.
             int count = 1;
-            return true;
+            //the current candy
+            RegularCandies currentCandy = RegularCandies.JellyBean;
+
+            for (int i=0; i<matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if(i+j !=0)
+                    {
+                        //if the candy is the same as in the matrix add 1 to counter
+                        if (currentCandy == matrix[i, j])
+                        {
+                            count++;
+                        }
+                        else if (count >= 3)
+                        {
+                            //to check if the counter reaches 3;
+                            return true;
+                        }
+                        else
+                        {
+                            //if its the same candies, set the counter to 1;
+                            count = 1;
+                        }
+                        
+                    }                    
+                    //set a new current candy
+                    currentCandy = matrix[i, j];
+
+                }
+            }
+            return false;
+            /*
+            if (count >= 3)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            */
+        }
+
+        bool ScoreColumnPresent(RegularCandies[,] matrix)
+        {
+            //this will be use as a counter.
+            int count = 1;
+            //the current candy
+            RegularCandies currentCandy = RegularCandies.JellyBean;
+
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    if (row + col != 0)
+                    {
+                        //if the candy is the same as in the matrix add 1 to counter
+                        if (currentCandy == matrix[row, col])
+                        {
+                            count++;
+                        }
+                        else if (count >= 3)
+                        {
+                            //to check if the counter reaches 3;
+                            return true;
+                        }
+                        else
+                        {
+                            //if its the same candies, set the counter to 1;
+                            count = 1;
+                        }
+
+                    }
+                    //set a new current candy
+                    currentCandy = matrix[row, col];
+                }
+            }
+            return false;
         }
     }
 }
